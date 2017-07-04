f45576798c2b37d67475c326765e100e64a2cd26
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2017-07-04 01:22:20 -0300

    removed unactive jpa repos

--- src/main/java/br/com/rafagonc/tjdata/repositories/ESAJNumberoPrimeriaInstanciaRepository.java
+++ src/main/java/br/com/rafagonc/tjdata/repositories/ESAJNumberoPrimeriaInstanciaRepository.java
@@ -1,10 +1,7 @@
 package br.com.rafagonc.tjdata.repositories;
 
-import br.com.rafagonc.tjdata.models.ESAJNumeroPrimeiraInstancia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 /**
  * Created by rafagonc on 29/06/17.
  */
-public interface ESAJNumberoPrimeriaInstanciaRepository extends JpaRepository<ESAJNumeroPrimeiraInstancia, Long> {
+public interface ESAJNumberoPrimeriaInstanciaRepository  {
 }
