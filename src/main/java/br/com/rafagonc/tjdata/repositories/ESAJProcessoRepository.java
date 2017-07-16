0817d8dfa24800730a46fbef401c644502899f4d
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2017-07-16 01:13:38 -0300

    tests added

--- src/main/java/br/com/rafagonc/tjdata/repositories/ESAJProcessoRepository.java
+++ src/main/java/br/com/rafagonc/tjdata/repositories/ESAJProcessoRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ESAJProcessoRepository extends JpaRepository<ESAJProcesso, Long> {
 
-    @Query("SELECT p FROM ESAJProcesso p WHERE p.numero LIKE :n")
+    @Query("SELECT p FROM ESAJProcesso p LEFT JOIN FETCH p.dadosProcessos d WHERE p.numero LIKE :n")
     ESAJProcesso getProcessoNumero(@Param("n") String numero);
 
 }
