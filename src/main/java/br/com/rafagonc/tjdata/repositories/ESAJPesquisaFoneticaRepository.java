4b0e359db29f81b1eb5a2c0cb1d3c489bace7744
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2017-08-15 23:51:00 -0300

    query

--- src/main/java/br/com/rafagonc/tjdata/repositories/ESAJPesquisaFoneticaRepository.java
+++ src/main/java/br/com/rafagonc/tjdata/repositories/ESAJPesquisaFoneticaRepository.java
@@ -2,9 +2,15 @@ package br.com.rafagonc.tjdata.repositories;
 
 import br.com.rafagonc.tjdata.models.ESAJPesquisaFonetica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by rafagonc on 13/08/17.
  */
 public interface ESAJPesquisaFoneticaRepository extends JpaRepository<ESAJPesquisaFonetica, Long> {
+
+    @Query("SELECT p FROM ESAJPesquisaFonetica p WHERE p.nome LIKE :np")
+    ESAJPesquisaFonetica findPesquisaFoneticaByNomeParte(@Param("np") String np);
+
 }
